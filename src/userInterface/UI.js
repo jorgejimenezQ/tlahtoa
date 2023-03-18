c5d7e7fbdbe7c45da0bf8790e58f6b03d88fa738
jorgejimenezQ <jorgejimenez@mail.weber.edu>
2023-03-18 11:42:23 -0600

    Download packages for the UI API and write boilerplate code.

---
+++ src/userInterface/UI.js
@@ -0,0 +1,22 @@
+import term from 'terminal-kit'
+
+const terminalKit = term.terminal
+
+export default class UI {
+    constructor() {}
+
+    // display the messages
+
+    // get input from user
+
+    // display ui (e.g., the menu)
+
+    // start the chat ui
+    /**
+     * Starts the UI.
+     * This method should be called after the UI object is created.
+     * It will display the UI and start listening for user input.
+     *
+     */
+    start() {}
+}
